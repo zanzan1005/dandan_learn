f36c6e2c2a060d5df49720274ceb646f1c9f5a59
zanzan1005 <936709325@qq.com>
2019-04-18 21:49:48 +0800

    wechatapp

---
+++ wxapp/LOL/pages/legends/legends.js
@@ -0,0 +1,18 @@
+Page({
+    data:{
+        legends:[
+            {
+                name:'阿卡丽',
+                title:'刺客'
+            },
+            {
+                name:'盖伦',
+                title:'战士'
+            },
+            {
+                name:'亚索',
+                title:'战士'
+            }
+        ]
+    }
+})
