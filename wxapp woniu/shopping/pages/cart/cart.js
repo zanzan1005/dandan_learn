c6733972828ecd5b8128ce0b6ba01a2298dc44fc
zanzan1005 <936709325@qq.com>
2019-05-10 21:31:08 +0800

    shopping

--- wxapp woniu/shopping/pages/cart/cart.js
+++ wxapp woniu/shopping/pages/cart/cart.js
@@ -8,33 +8,77 @@ Page({
     hasList:false,
     carts:[],
     selectAllStatus:true
+    
   },
   selectAll(e){
     let selectAllStatus = this.data.selectAllStatus;
     selectAllStatus = !selectAllStatus;
     let carts = this.data.carts;
-    for(let i = 0;i < carts.length;i++){
-      carts[i].selected = selectAllStatus
+    for(let i = 0; i < carts.length; i++){
+      carts[i].selected = selectAllStatus;
     }
     this.setData({
       selectAllStatus:selectAllStatus,
       carts:carts
     })
   },
-
+  getTotalPrice(){
+    let carts = this.data.carts
+    let total = 0
+    for(let i = 0; i<carts.length;i++){
+      if (carts[i].selected){
+        total += carts[i].num * carts[i].price
+      }
+    }
+    this.setData({
+      totalPrice:total.toFixed(2)
+    })
+  },
+  minusCount(e){
+    // console.log(e)
+    const index = e.target.dataset.index;
+    let carts = this.data.carts;
+    let num = carts[index].num;
+    if (num <=1){
+      return;
+    } 
+    num = num - 1;
+    carts[index].num = num;
+    this.setData({
+      carts:carts
+    })
+    this.getTotalPrice()
+  },
+  addCount(e){
+    // console.log(e)
+    const index = e.target.dataset.index;
+    let carts = this.data.carts;
+    let num = carts[index].num;
+    num = num + 1;
+    carts[index].num = num;
+    this.setData({
+      carts:carts
+    })
+    this.getTotalPrice()
+  },
+  deleteList(e){
+    const index = e.target.dataset.index;
+    const carts = this.data.carts; 
+    carts.splice(index,1);
+    this.setData({
+      carts:carts
+    })
+    if(!carts.length){
+      hasList:false
+    } else {
+      this.getTotalPrice()
+    }
+  },
   /**
    * 生命周期函数--监听页面加载
    */
   onLoad: function (options) {
-    setTimeout(() =>{
-      this.setData({
-        hasList:true,
-        carts:[
-          { id: 1, title: '新鲜芹菜 半斤', image: '/image/s5.png', num: 4, price: 0.01, selected: true },
-          { id: 2, title: '素米 500g', image: '/image/s6.png', num: 1, price: 0.03, selected: true }
-        ]
-      })
-    },1000)
+
   },
 
   /**
@@ -48,7 +92,16 @@ Page({
    * 生命周期函数--监听页面显示
    */
   onShow: function () {
-
+    setTimeout(()=>{
+      this.setData({
+        hasList:true,
+        carts:[
+          { id: 1, title: '新鲜芹菜 半斤', image: '/image/s5.png', num: 4, price: 0.01, selected: true },
+          { id: 2, title: '素米 500g', image: '/image/s6.png', num: 1, price: 0.03, selected: true }
+        ]
+      });
+      this.getTotalPrice()
+    },1000)
   },
 
   /**
