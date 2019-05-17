5e30549d10515e4bafec2d095c8eac66aaf19227
zanzan1005 <936709325@qq.com>
2019-05-17 12:08:21 +0800

    0

--- vant-cloud/cloudfunctions/getGroup/index.js
+++ vant-cloud/cloudfunctions/getGroup/index.js
@@ -1,16 +1,35 @@
 // 云函数入口文件
 const cloud = require('wx-server-sdk')
-
+const env = 'goudan-nswye'
 cloud.init()
+const db = cloud.database({ env })
 
 // 云函数入口函数
 exports.main = async (event, context) => {
-  const wxContext = cloud.getWXContext()
+const openId = cloud.getWXContext().openId
 
-  return {
-    event,
-    openid: wxContext.OPENID,
-    appid: wxContext.APPID,
-    unionid: wxContext.UNIONID,
+let groupList = await db.collection('user-group')
+.where({
+  userId:openId
+})
+.get()
+let returnResult = []
+for(let item of groupList.data){
+  const oneGroup = await db.collection('group')
+  .where({
+    _id:item.groupId,
+    deleted:false
+  })
+  .get()
+  if(oneGroup.data.length > 0){
+    const userInfo = await db.collection('user')
+    .where({
+      openId:oneGroup.data[0].createBy
+    }).get()
+    oneGroup.data[0].createBy = userInfo.data[0]
+    oneGroup.data[0].relateUserGroupId = item._id
+    returnReault.push(oneGroup.data[0])
   }
+}
+return returnResult.sort((a,b) =>a.createTime < b.createTime ? 1:-1)
 }
