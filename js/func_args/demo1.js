2e9b983c8a5feb313226e8a2ac10c0a707dbbb1e
zanzan1005 <936709325@qq.com>
2019-05-21 21:32:30 +0800

    521

---
+++ js/func_args/demo1.js
@@ -0,0 +1,6 @@
+function information(name,sex,money,birthday,color,likes,follow,age){
+  return `${name} ${sex} ${money} ${birthday} ${color} ${likes} ${follow} ${age}`
+}
+let user = {name:'小鲤鱼',sex:'女',money:'一亿美元',birthday:'5.22',color:'黄',likes:['看剧'],follow:'何洛洛',age:22};
+// let name = name;
+information(user);
