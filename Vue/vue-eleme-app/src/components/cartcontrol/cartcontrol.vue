fd2a203bbb910fede4866b27142e6ed1352bf76a
zanzan1005 <936709325@qq.com>
2019-06-13 18:13:17 +0800

    app.vue

--- Vue/vue-eleme-app/src/components/cartcontrol/cartcontrol.vue
+++ Vue/vue-eleme-app/src/components/cartcontrol/cartcontrol.vue
@@ -1,23 +1,51 @@
 <template>
   <div class="cartcontrol">
     <transition name="move">
-      <div class="cart-decrease">
+      <div class="cart-decrease" v-show="food.count > 0" @click.stop.prevent="decreaseCart">
         <span class="inner icon-remove_circle_outline"></span>
       </div>
     </transition>
-    <div class="cart-count">123</div>
-    <div class="cart-add icon-add_circle"></div>
-    </div>
+    <div class="cart-count" v-show="food.count > 0">{{food.count}}</div>
+    <div class="cart-add icon-add_circle" @click.stop.prevent="addCart"></div>
+  </div>
 </template>
 
 <script>
 export default {
-  name: 'cartcontrol',
+  name: "cartcontrol",
+  props: {
+    food: {
+      type: Object
+    }
+  },
   data () {
     return {
 
     }
-  }
+  },
+  methods: {
+    addCart (event) {
+      // console.log(event)
+      if(!event._constructed) {//如果不存在这个属性，则为原生点击事件，不执行下面的函数
+        return
+      }
+      if (!this.food.count) {
+        this.$set(this.food, 'count', 1)
+      }else{
+        this.food.count++
+      }
+      this.$emit('add', event.target)
+    },
+    decreaseCart () {
+      if(!event._constructed) {//如果不存在这个属性，则为原生点击事件，不执行下面的函数
+        return
+      }
+      if (this.food.count) {
+        this.food.count--
+      }
+    }
+  },
+
 }
 </script>
 
@@ -70,4 +98,3 @@ export default {
   
 
 </style>
-
