ae64c1f09609741d82897a24ae3dcc20ab4edc37
zanzan1005 <936709325@qq.com>
2019-06-15 20:55:43 +0800

    vuex

---
+++ Vue/todomvc/src/App.vue
@@ -0,0 +1,33 @@
+<template>
+  <div>
+    <counter/>
+    <!-- <counter-brother :count="count"/> -->
+  </div>
+</template>
+
+<script>
+//两个兄弟组件，共享count 数据
+//count 不应该属于Counter组件私有，data(){} ,props
+import Counter from './components/Counter';
+// import CounterBrother from './components/CounterBrother';
+export default {
+  data () {
+    return {
+      count: 0
+    }
+  },
+  methods: {
+    incrementCount () {
+      this.count++;
+    }
+  },
+  components: {
+    'counter': Counter,
+    // 'counter-brother': CounterBrother
+  }
+}
+</script>
+
+<style>
+
+</style>
