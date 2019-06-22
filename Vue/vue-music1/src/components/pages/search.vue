d7837b8f48182cf5f86a19b40081871d81f574c5
zanzan1005 <936709325@qq.com>
2019-06-22 17:15:36 +0800

    search

---
+++ Vue/vue-music1/src/components/pages/search.vue
@@ -0,0 +1,22 @@
+<template>
+  <div class="search">
+    <div class="search-box-wrapper">
+      <!-- 搜索框 -->
+      <router-view></router-view>
+      <v-searchBox></v-searchBox>
+    </div>
+  </div>
+</template>
+
+<script>
+import searchBox from '@/components/searchBox'
+export default {
+  components: {
+    'v-searchBox': searchBox
+  }
+}
+</script>
+
+<style>
+
+</style>
