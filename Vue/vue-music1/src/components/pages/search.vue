797bcbe3be48b75d867111b931cf1954057115ae
zanzan1005 <936709325@qq.com>
2019-06-24 12:04:22 +0800

    music

--- Vue/vue-music1/src/components/pages/search.vue
+++ Vue/vue-music1/src/components/pages/search.vue
@@ -5,18 +5,59 @@
       <router-view></router-view>
       <v-searchBox></v-searchBox>
     </div>
+    <div class="shortcut-wrapper" ref="shortcutWrapper" v-show="!query">
+      <!-- 热门搜索 -->
+      <v-scroll class="shortcut" ref="shortcut" :data="shortcut" :refeshDelay="refreshDelay">
+        <div>
+          <div class="hot-key">
+            <h1 class="title">热门搜索</h1>
+            <ul>
+              <li class="item" v-for="(item,index) in hotKey" :key="index" @click="addQuery(item.first)">
+                <span>{{item.first}}</span>
+              </li>
+            </ul>
+          </div>
+        </div>
+      </v-scroll>
+    </div>
   </div>
 </template>
 
 <script>
 import searchBox from '@/components/searchBox'
+import scroll from '@/components/scroll'
 export default {
+  data () {
+    return {
+       query: false,
+       shortcut: [
+         
+       ],
+       refreshDelay: 0,
+       hotKey: [
+         {
+           first:'许嵩1新歌发布'
+         },
+         {
+           first:'2新歌发布'
+         },
+         {
+           first:'3新歌发布'
+         },
+         {
+           first:'许嵩4新歌发布'
+         }
+       ]
+    }
+  },
   components: {
-    'v-searchBox': searchBox
+    'v-searchBox': searchBox,
+    'v-scroll':scroll
   }
 }
 </script>
 
 <style>
+
 
 </style>
