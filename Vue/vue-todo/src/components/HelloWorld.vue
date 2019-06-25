d7b679b5dddba662feada503a631fa83bae0bf37
zanzan1005 <936709325@qq.com>
2019-06-25 23:27:28 +0800

    full

---
+++ Vue/vue-todo/src/components/HelloWorld.vue
@@ -0,0 +1,47 @@
+<template>
+  <div class="hello">
+    <h1>{{ msg }}</h1>
+    <h2>Essential Links</h2>
+    <ul class="nav nav-tabs">
+      <li role="presentation" class="active">
+        <a href="#">Home</a>
+      </li>
+      <li role="presentation">
+        <a href="#">Profile</a>
+      </li>
+      <li role="presentation">
+        <a href="#">Messages</a>
+      </li>
+    </ul>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "HelloWorld",
+  data() {
+    return {
+      msg: "Welcome to Your Vue.js App"
+    };
+  }
+};
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+h1,
+h2 {
+  font-weight: normal;
+}
+ul {
+  list-style-type: none;
+  padding: 0;
+}
+li {
+  display: inline-block;
+  margin: 0 10px;
+}
+a {
+  color: #42b983;
+}
+</style>
