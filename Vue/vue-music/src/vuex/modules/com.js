2b7769147928ac7d3edd78d9086bd99d3107c83c
zanzan1005 <936709325@qq.com>
2019-06-26 14:45:33 +0800

    music

---
+++ Vue/vue-music/src/vuex/modules/com.js
@@ -0,0 +1,38 @@
+import api from '../../api'
+import * as types from '../types'
+
+const state = {
+  showSidebar: false,
+  fullScreen: false
+}
+
+const mutations = {
+  [types.COM_SHOW_SIDE_BAR] (state, status) {
+    state.showSidebar = status
+  },
+  [types.SET_FULL_SCREEN] (state, status) {
+    state.fullScreen = status
+  }
+}
+
+const actions = {
+  setShowSidebar ({commit}, status) {
+    commit(types.COM_SHOW_SIDE_BAR, status)
+  },
+  selectPlaySong ({ commit }, status) {
+    // let playlist = state.playlist.slice()
+    commit(types.SET_FULL_SCREEN, status)
+  }
+}
+
+const getters = {
+  showSidebar: state => state.showSidebar,
+  fullScreen: state => state.fullScreen
+}
+
+export default {
+  state,
+  mutations,
+  actions,
+  getters
+}
