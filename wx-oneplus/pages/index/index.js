a1028bd21d660846d264debd604c29e4b7133140
zanzan1005 <936709325@qq.com>
2019-06-30 00:48:28 +0800

    app

--- wx-oneplus/pages/index/index.js
+++ wx-oneplus/pages/index/index.js
@@ -8,15 +8,22 @@ Page({
       '../../images/1.jpg',
       '../../images/2.jpg'
     ],
-    indicatorDots: true, 
-    autoplay: true, 
-    interval: 3000, 
-    duration: 1000, 
+    indicatorDots: true,
+    autoplay: true,
+    interval: 3000,
+    duration: 1000,
     circular: true,
-    index: 1
- },
-  
+    index: 1,
+    navbar: ['商品精选', '耳机', '壳/膜','适配器/数据线','套装','生活馆'],
+    currentTab: 0
+  },
+  navbarTap: function(e){
+    this.setData({
+      currentTab: e.currentTarget.dataset.idx
+    })
+  },
+
   onLoad: function () {
- 
+
   }
 })
