b35eca09df2a9cdc2863158cb9aff0760022c9a0
zanzan1005 <936709325@qq.com>
2019-06-29 16:38:02 +0800

    wx

--- wx-oneplus/pages/index/index.js
+++ wx-oneplus/pages/index/index.js
@@ -4,27 +4,19 @@ const app = getApp()
 
 Page({
   data: {
-    // banner
     imgUrls: [
       '../../images/1.jpg',
-      '../../images/2.jpg',
+      '../../images/2.jpg'
     ],
-    indicatorDots: true, //是否显示面板指示点
-    autoplay: true, //是否自动切换
-    interval: 3000, //自动切换时间间隔,3s
-    duration: 1000, //  滑动动画时长1s
-    circular: true //是否采用衔接滑动
+    indicatorDots: true, 
+    autoplay: true, 
+    interval: 3000, 
+    duration: 1000, 
+    circular: true,
+    index: 1
  },
-  //事件处理函数
+  
   onLoad: function () {
  
-  },
-  getUserInfo: function(e) {
-    console.log(e)
-    app.globalData.userInfo = e.detail.userInfo
-    this.setData({
-      userInfo: e.detail.userInfo,
-      hasUserInfo: true
-    })
   }
 })
