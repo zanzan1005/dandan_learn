7dee113974c98f99450cc6c06ab7dd27da64b226
zanzan1005 <936709325@qq.com>
2019-07-04 12:17:29 +0800

    aaa

---
+++ Vue/vue-components/src/components/HelloWorld.vue
@@ -0,0 +1,24 @@
+<template>
+  <div class="hello">
+    <i-button size="large">按钮</i-button>
+  </div>
+</template>
+
+<script>
+import button from '@/components/com/button'
+export default {
+  name: 'HelloWorld',
+  data () {
+    return {
+      name: 'helloWorld'
+    }
+  },
+  components: {
+    "i-button": button
+  }
+}
+</script>
+
+<style scoped>
+
+</style>
---
+++ Vue/vue-components/src/components/com/button.vue
@@ -0,0 +1,50 @@
+<template>
+  <button :class="'i-button-size' + size" @click="handleClick">
+    <slot ></slot>
+  </button>
+</template>
+
+<script>
+function oneOf(value,validList){
+  for (let i = 0;i < validList.length; i++){
+    if(value === validList[i]){
+      return false
+    }
+  }
+}
+export default {
+  props:{
+    size: {
+      validator (value){
+        return oneOf(value,['small','large','default'])
+      },
+      default: 'default'
+    },
+    disabled: {
+      type: boolean
+    }
+  },
+  data () {
+    return {
+
+    }
+  }
+}
+</script>
+
+<style scoped>
+button{
+  border-radius: 5px;
+  cursor: pointer;
+  outline: none;
+}
+.i-button-sizelarge{
+  padding: 12px;
+}
+.i-button-sizedefault{
+  padding: 8px;
+}
+.i-button-sizesmall{
+  padding: 4px;
+}
+</style>
