1c3514715e1b4c69c6fefadb3bd4dcaa3313f198
zanzan1005 <936709325@qq.com>
2019-07-08 16:59:37 +0800

    redux

---
+++ Taro/todoList/src/store/index.js
@@ -0,0 +1,12 @@
+import { createStore,applyMiddleware } from 'redux'
+import rootReducer from '../reducers'
+
+export default function configStore () {
+  // createStore 返回实例 单一状态树
+  //vuex 四个部分
+  //redux state reducer(函数) 概念
+  //action 返回新的状态 没有mutations
+  const store = createStore(rootReducer);
+  return store
+
+}
