6cd4fb9cfac2181f45278ef55f7f4357710d5687
zanzan1005 <936709325@qq.com>
2019-07-15 10:07:41 +0800

    aha

--- React/react-demo/src/App.js
+++ React/react-demo/src/App.js
@@ -38,20 +38,32 @@ class App extends Component {
       })
     }
   render() { 
+     const style = {
+      border: "1px solid #c0c0c0",
+      textAlign: "center",
+      fontSize: "15px",
+      height: "50px",
+      width: "250px"
+      };
     return ( 
+     
       <div className="App">
         <h1>今天吃了火腿肠</h1>
         {/* <button onClick={() => this.changeState("goudan")} >更改数据源</button> */}
         <button onClick={this.changeState.bind(this,"missu")}>更改数据</button>
         {/* <button onClick={() => this.changeCount(1222)}>更改数字</button> */}
-        <button onClick={this.changeCount.bind(this,122225)}>更改数字</button>
-        
-      <Person changed = {this.nameChange}
-      name = {this.state.Person[0].name} count = {this.state.Person[0].count}/>
-      <Person myClick={this.changeCount.bind(this,25484)}
-      name = {this.state.Person[1].name} count = {this.state.Person[1].count}/>
-      <Person name = {this.state.Person[2].name} count = {this.state.Person[2].count}/>
-      <Person>肖战最帅！！！</Person>
+        <button style={style}
+        onClick={this.changeCount.bind(this,122225)}>更改数字</button>
+        {
+       <div> 
+        <Person changed = {this.nameChange}
+        name = {this.state.Person[0].name} count = {this.state.Person[0].count}/>
+        <Person myClick={this.changeCount.bind(this,25484)}
+        name = {this.state.Person[1].name} count = {this.state.Person[1].count}/>
+        <Person name = {this.state.Person[2].name} count = {this.state.Person[2].count}/>
+        <Person>肖战最帅！！！</Person>
+      </div>
+        }
     </div>
      );
   }
--- React/react-demo/src/Person/Person.js
+++ React/react-demo/src/Person/Person.js
@@ -1,10 +1,11 @@
 import React from 'react';
+import './Person.css'
 
 const Person = (props) => {
   return(
-    <div>
+    <div className="Person">
       <p onClick={props.myClick}>我是{props.name},我有{props.count}块钱</p>
-      <input type="text" onChange = {props.changed}/>
+      <input type="text" onChange = {props.changed} defaultValue={props.name}/>
       <p>{props.children}</p>
     </div>
   )
