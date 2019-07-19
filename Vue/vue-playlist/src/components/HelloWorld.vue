cf562af9227ece97918972c3bffb9ee9b04259ae
zanzan1005 <936709325@qq.com>
2019-07-19 23:07:09 +0800

    rename

--- Vue/vue-playlist/src/components/HelloWorld.vue
+++ Vue/vue-playlist/src/components/HelloWorld.vue
@@ -1,8 +1,16 @@
 <template>
   <div class="hello">
-    <p>name: {{ this.name }}</p>
-    <p>job: {{ this.job }}</p>
-    <h1>{{ greet() }}</h1>
+    <p>name: {{ name }}</p>
+    <p>job: {{ job }}</p>
+    <h1>{{ greet("noon") }}</h1>
+    <input type="text" v-bind:value="job">
+    <a v-bind:href="website">腾讯课堂</a>
+    <p v-html="websiteTage"></p>
+    <button v-on:click="add(1)">加一岁</button>
+    <button v-on:click="reduce(1)">减一岁</button>
+    <button v-on:click="add(10)">加10岁</button>
+    <button v-on:click="reduce(10)">减10岁</button>
+    <p>My age is {{age}}</p>
   </div>
 </template>
 
@@ -11,14 +19,22 @@ export default {
   name: 'HelloWorld',
   data () {
     return {
+      age: 20,
       name: '狗蛋',
       job: 'web开发',
-      website: 'https://oceanwu.ke.qq.com/'
+      website: 'https://oceanwu.ke.qq.com/',
+      websiteTage: "<a href='https://oceanwu.ke.qq.com/'>腾讯课堂</a>"
     } 
   },
   methods: {
-    greet: function () {
-      return "Good night"
+    greet: function (time) {
+      return "Good " + " " + time + " " + "!"
+    },
+    add: function (add) {
+      this.age += add
+    },
+    reduce: function (red) {
+      this.age -= red
     }
   }
 }
@@ -26,18 +42,5 @@ export default {
 
 <!-- Add "scoped" attribute to limit CSS to this component only -->
 <style scoped>
-h1, h2 {
-  font-weight: normal;
-}
-ul {
-  list-style-type: none;
-  padding: 0;
-}
-li {
-  display: inline-block;
-  margin: 0 10px;
-}
-a {
-  color: #42b983;
-}
+
 </style>
