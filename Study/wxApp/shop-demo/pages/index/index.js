07f9e5db072c15d312aa3eff3f98048e05b9d972
zanzan1005 <936709325@qq.com>
2019-07-28 11:55:24 +0800

    米快画

--- Study/wxApp/shop-demo/pages/index/index.js
+++ Study/wxApp/shop-demo/pages/index/index.js
@@ -9,6 +9,17 @@ Page({
       "../../images/2.jpg",
       "../../images/3.jpg"
     ],
+    propsList: [
+      {name: '富安娜(FUANNA)',price: '￥369.00'},
+      {name: '富安娜(FUANNA)',price: '￥368.00'},
+      {name: '富安娜(FUANNA)',price: '￥367.00'},
+      {name: '富安娜(FUANNA)',price: '￥366.00'},
+      {name: '富安娜(FUANNA)',price: '￥365.00'},
+      {name: '富安娜(FUANNA)',price: '￥364.00'},
+      {name: '富安娜(FUANNA)',price: '￥363.00'},
+      {name: '富安娜(FUANNA)',price: '￥362.00'},
+      {name: '富安娜(FUANNA)',price: '￥361.00'}
+    ],
     indicatorDots: true,
     autoplay: true,
     interval: 4000,
