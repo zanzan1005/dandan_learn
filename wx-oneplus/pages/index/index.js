70177b59e7de306c6a801b6bfdb50553303aa371
zanzan1005 <936709325@qq.com>
2019-07-28 11:54:46 +0800

    wx:for

--- wx-oneplus/pages/index/index.js
+++ wx-oneplus/pages/index/index.js
@@ -8,6 +8,26 @@ Page({
       '../../images/1.jpg',
       '../../images/2.jpg'
     ],
+    productLists: [
+      {src:"../../images/title.jpg",title:"Oneplus 7 Pro",price:"最低价 ￥3999"},
+      {src:"../../images/title2.jpg",title:"Oneplus 7",price:"最低价 ￥2999"},
+      {src:"../../images/title3.jpg",title:"Oneplus 7 加油套装",price:"最低价 ￥345.1",minPrice:"最低价 ￥ 406"},
+      {src:"../../images/title4.jpg",title:"Oneplus 7 Pro个性保护壳",price:"最低价 ￥99"},
+      {src:"../../images/title5.jpg",title:"Oneplus 7 Pro全包保护壳",price:"最低价 ￥199"},
+      {src:"../../images/title6.jpg",title:"一加云耳 2",price:"￥599"},
+      {src:"../../images/title7.jpg",title:"OnePlus Warp 30 闪充套装",price:"￥217",minPrice:"￥228"},
+      {src:"../../images/title8.jpg",title:"OnePlus 7 Pro 全家桶套装",price:"最低价 ￥421.6",minPrice:" 最低价 ￥496"},
+      {src:"../../images/title10.jpg",title:"OnePlus 7 Pro 悦耳套装",price:"最低价 ￥254.6",minPrice:" 最低价 ￥268"},
+      {src:"../../images/title9.jpg",title:"一加Wrap闪充30电源适配器",price:"￥149"},
+      {src:"../../images/title11.jpg",title:"一加Wrap闪充30车载充电器",price:"￥199"},
+      {src:"../../images/title12.jpg",title:"一加Wrap闪充30 Type-C 数据线",price:"最低价 ￥79"},
+
+
+
+
+
+
+    ],
     indicatorDots: true,
     autoplay: true,
     interval: 3000,
