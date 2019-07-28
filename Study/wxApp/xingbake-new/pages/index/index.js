a8c06a09c9c5c7d88ad13d6d2a72961fd2a6ccb7
zanzan1005 <936709325@qq.com>
2019-07-29 00:47:09 +0800

    old-xingbake

---
+++ Study/wxApp/xingbake-new/pages/index/index.js
@@ -0,0 +1,18 @@
+//index.js
+//获取应用实例
+const app = getApp()
+
+Page({
+  data: {
+    imgUrls: [
+      '../../images/banner1.jpg',
+      '../../images/banner2.jpg',
+      '../../images/banner3.jpg'
+    ],
+    indicatorDots: true,
+    autoplay: true,
+    circular: true,
+    interval: 2000,
+    duration: 1000
+  },
+})
