7f3bd50aab984e2944c814bf1d12dbe1c7bb90c4
zanzan1005 <936709325@qq.com>
2019-08-11 14:30:27 +0800

    a

---
+++ Study/demo/src/components/header.vue
@@ -0,0 +1,45 @@
+<template>
+  <div class="header">
+    <div class="title">
+      <router-link to="/">
+        豆瓣
+      </router-link>
+    </div>
+    <div class="lists">
+      <ul>
+        <li>
+          <router-link to="/">电影</router-link>
+        </li>
+        <li>
+          <router-link to="/">图书</router-link>
+        </li>
+        <li>
+          <router-link to="/">广播</router-link>
+        </li>
+        <li>
+          <router-link to="/">小组</router-link>
+        </li>
+      </ul>
+    </div>
+    <span class="search-icon" @click="showSearchBtn"></span>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'header',
+  data () {
+    return {
+
+    }
+  },
+  methods: {
+    showSearchBtn () {}
+  }
+}
+</script>
+
+<style lang="stylus" scoped>
+
+  
+</style>
